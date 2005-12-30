96086e1a2b6b6461f86dc328f15784109195c064
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2005-12-30 19:55:43 +0000

    Huh, no C errors

--- ismicro-proximity/px-core/src/java/hu/ismicro/commons/proximity/base/StorageException.java
+++ ismicro-proximity/px-core/src/java/hu/ismicro/commons/proximity/base/StorageException.java
@@ -1,15 +1,13 @@
 package hu.ismicro.commons.proximity.base;
 
-import hu.ismicro.commons.proximity.ProximityException;
+public class StorageException extends RuntimeException {
 
-public class StorageException extends ProximityException {
-    
-    public StorageException(String msg) {
-        super(msg);
-    }
+	public StorageException(String msg) {
+		super(msg);
+	}
 
-    public StorageException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
+	public StorageException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
 
 }
