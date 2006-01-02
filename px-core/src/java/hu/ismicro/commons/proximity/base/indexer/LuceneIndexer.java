7bc53ad465d0f714a280a29519838acb4a97fa30
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-01-02 16:40:49 +0000

    Separated logic

--- px-core/src/java/hu/ismicro/commons/proximity/base/indexer/LuceneIndexer.java
+++ px-core/src/java/hu/ismicro/commons/proximity/base/indexer/LuceneIndexer.java
@@ -1,11 +1,17 @@
 package hu.ismicro.commons.proximity.base.indexer;
 
-import java.util.List;
-
 import hu.ismicro.commons.proximity.ItemProperties;
 import hu.ismicro.commons.proximity.base.Indexer;
 
+import java.util.List;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+
 public class LuceneIndexer implements Indexer {
+	
+	private IndexReader indexReader;
+	private IndexWriter indexWriter;
 
 	public void addItemProperties(ItemProperties ip) {
 		// TODO Auto-generated method stub
