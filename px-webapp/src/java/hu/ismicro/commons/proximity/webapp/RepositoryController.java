8e69b71697a4a555eb5c0bc9eae9a5eed2461339
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-01-03 23:46:39 +0000

    Fixed repoId bug for retrieval

--- px-webapp/src/java/hu/ismicro/commons/proximity/webapp/RepositoryController.java
+++ px-webapp/src/java/hu/ismicro/commons/proximity/webapp/RepositoryController.java
@@ -23,81 +23,87 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class RepositoryController extends MultiActionController {
 
-    private Proximity proximity;
+	private Proximity proximity;
 
-    public void setProximity(Proximity proximity) {
-        this.proximity = proximity;
-    }
+	public void setProximity(Proximity proximity) {
+		this.proximity = proximity;
+	}
 
-    public Proximity getProximity() {
-        return proximity;
-    }
+	public Proximity getProximity() {
+		return proximity;
+	}
 
-    public ModelAndView repositoryList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String requestURI = request.getRequestURI().substring(
-                request.getContextPath().length() + request.getServletPath().length());
-        if (requestURI.length() == 0) {
-            requestURI = "/";
-        }
-        logger.info("Got repository request on URI " + requestURI);
-        String orderBy = request.getParameter("orderBy") == null ? "name" : request.getParameter("orderBy");
-        String targetRepository = request.getParameter("repositoryId");
+	public ModelAndView repositoryList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String requestURI = request.getRequestURI().substring(
+				request.getContextPath().length() + request.getServletPath().length());
+		if (requestURI.length() == 0) {
+			requestURI = "/";
+		}
+		logger.info("Got repository request on URI " + requestURI);
+		String orderBy = request.getParameter("orderBy") == null ? "name" : request.getParameter("orderBy");
+		String targetRepository = request.getParameter("repositoryId");
 
-        ItemProperties itemProps = null;
-        try {
-            if (targetRepository == null) {
-                logger.info("Got request for repository on URI: " + requestURI);
-                itemProps = proximity.retrieveItemProperties(requestURI);
-            } else {
-                logger.info("Got request for repository on URI: " + requestURI + " from repository id "
-                        + targetRepository);
-                itemProps = proximity.retrieveItemPropertiesFromRepository(requestURI, targetRepository);
-            }
+		ItemProperties itemProps = null;
+		try {
+			if (targetRepository == null) {
+				logger.info("Got request for repository on URI: " + requestURI);
+				itemProps = proximity.retrieveItemProperties(requestURI);
+			} else {
+				logger.info("Got request for repository on URI: " + requestURI + " from repository id "
+						+ targetRepository);
+				itemProps = proximity.retrieveItemPropertiesFromRepository(requestURI, targetRepository);
+			}
 
-            if (itemProps.isDirectory()) {
-                List items = null;
-                if (targetRepository == null) {
-                    items = proximity.listItems(requestURI);
-                } else {
-                    items = proximity.listItemsFromRepository(requestURI, targetRepository);
-                }
-                PropertyComparator.sort(items, new MutableSortDefinition(orderBy, true, true));
-                Map result = new HashMap();
-                result.put("items", items);
-                result.put("orderBy", orderBy);
-                result.put("requestUri", requestURI);
-                result.put("requestPathList", explodeUriToList(requestURI));
-                return new ModelAndView("repository/repositoryList", result);
-            } else {
-                Item item = proximity.retrieveItem(requestURI);
-                // TODO: Made this proper (content type by ext, size, etc...)
-                response.setContentType("application/octet-stream");
-                response.setContentLength((int)item.getProperties().getSize());
-                response.setDateHeader("Last-Modified", item.getProperties().getLastModified().getTime());
-                InputStream is = item.getStream();
-                OutputStream os = response.getOutputStream();
-                IOUtils.copy(is, os);
-                is.close();
-                return null;
-            }
-        } catch (ItemNotFoundException ex) {
-            logger.info("Item not found on URI " + requestURI);
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return null;
-        }
-    }
+			if (itemProps.isDirectory()) {
+				List items = null;
+				if (targetRepository == null) {
+					items = proximity.listItems(requestURI);
+				} else {
+					items = proximity.listItemsFromRepository(requestURI, targetRepository);
+				}
+				PropertyComparator.sort(items, new MutableSortDefinition(orderBy, true, true));
+				Map result = new HashMap();
+				result.put("items", items);
+				result.put("orderBy", orderBy);
+				result.put("requestUri", requestURI);
+				result.put("requestPathList", explodeUriToList(requestURI));
+				return new ModelAndView("repository/repositoryList", result);
+			} else {
+				Item item = null;
+				// TODO: Optimize? Maybe a search first?
+				if (targetRepository == null) {
+					item = proximity.retrieveItem(requestURI);
+				} else {
+					item = proximity.retrieveItemFromRepository(requestURI, targetRepository);
+				}
+				// TODO: Made this proper (content type by ext, size, etc...)
+				response.setContentType("application/octet-stream");
+				response.setContentLength((int) item.getProperties().getSize());
+				response.setDateHeader("Last-Modified", item.getProperties().getLastModified().getTime());
+				InputStream is = item.getStream();
+				OutputStream os = response.getOutputStream();
+				IOUtils.copy(is, os);
+				is.close();
+				return null;
+			}
+		} catch (ItemNotFoundException ex) {
+			logger.info("Item not found on URI " + requestURI);
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+		}
+	}
 
-    protected List explodeUriToList(String uri) {
-        List result = new ArrayList();
-        String[] explodedUri = uri.split("/");
-        StringBuffer sb = new StringBuffer("/");
-        for (int i = 0; i < explodedUri.length; i++) {
-            if (explodedUri[i].length() > 0) {
-                result.add(sb + explodedUri[i]);
-                sb.append(explodedUri[i] + "/");
-            }
-        }
-        return result;
-    }
+	protected List explodeUriToList(String uri) {
+		List result = new ArrayList();
+		String[] explodedUri = uri.split("/");
+		StringBuffer sb = new StringBuffer("/");
+		for (int i = 0; i < explodedUri.length; i++) {
+			if (explodedUri[i].length() > 0) {
+				result.add(sb + explodedUri[i]);
+				sb.append(explodedUri[i] + "/");
+			}
+		}
+		return result;
+	}
 
 }
