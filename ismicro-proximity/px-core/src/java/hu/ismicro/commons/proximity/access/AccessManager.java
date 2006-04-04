75b8cc1c178791e2081504876399c5ebff6abda7
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-04-04 14:21:49 +0000

    Added comments

--- ismicro-proximity/px-core/src/java/hu/ismicro/commons/proximity/access/AccessManager.java
+++ ismicro-proximity/px-core/src/java/hu/ismicro/commons/proximity/access/AccessManager.java
@@ -5,8 +5,23 @@ import hu.ismicro.commons.proximity.ProximityRequest;
 
 import java.util.Map;
 
+/**
+ * Interface for access manager.
+ * 
+ * @author t.cservenak
+ *
+ */
 public interface AccessManager {
 	
+	/**
+	 * The implementation of this method should throw AccessDeniedException or any
+	 * subclass if it denies access.
+	 * 
+	 * @param grantee
+	 * @param request
+	 * @param config
+	 * @throws AccessDeniedException
+	 */
 	void decide(Object grantee, ProximityRequest request, Map config) throws AccessDeniedException;
 
 }
