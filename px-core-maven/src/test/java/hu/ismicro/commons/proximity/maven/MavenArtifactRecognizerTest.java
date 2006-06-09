7d84e638c6ccea8f9cb68cb10b3f4a1fa5922380
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-06-09 00:09:49 +0000

    Added JUnit test

---
+++ px-core-maven/src/test/java/hu/ismicro/commons/proximity/maven/MavenArtifactRecognizerTest.java
@@ -0,0 +1,29 @@
+package hu.ismicro.commons.proximity.maven;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class MavenArtifactRecognizerTest extends TestCase {
+    
+    public void testIsPom() {
+        Assert.assertEquals(true, MavenArtifactRecognizer.isPom("aaa.pom"));
+        Assert.assertEquals(true, MavenArtifactRecognizer.isPom("zxc-1-2-3.pom"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isPom("aaa.jar"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isPom("aaa.pom-a"));
+    }
+
+    public void testIsSnapshot() {
+        Assert.assertEquals(true, MavenArtifactRecognizer.isSnapshot("xsd-SNAPSHOT.jar"));
+        Assert.assertEquals(true, MavenArtifactRecognizer.isSnapshot("xsd-SNAPSHOT.pom"));
+        Assert.assertEquals(true, MavenArtifactRecognizer.isSnapshot("/a/b/c/xsd-1.2.3-SNAPSHOT.pom"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isSnapshot("xsd-SNAPsHOT.jar"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isSnapshot("xsd-SNAPHOT.pom"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isSnapshot("/a/b/c/xsd-1.2.3NAPSHOT.pom"));
+    }
+
+    public void testIsMetadata() {
+        Assert.assertEquals(true, MavenArtifactRecognizer.isMetadata("maven-metadata.xml"));
+        Assert.assertEquals(false, MavenArtifactRecognizer.isMetadata("aven-metadata.xml"));
+    }
+
+}
