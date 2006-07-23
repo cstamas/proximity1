f5149215de4ab7242891f6ab17dbb90f874a6a46
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-07-23 22:46:12 +0000

    Preliminary tests from remoteFTPPeer

--- px-core/src/main/java/hu/ismicro/commons/proximity/base/remote/CommonsNetFtpRemotePeer.java
+++ px-core/src/main/java/hu/ismicro/commons/proximity/base/remote/CommonsNetFtpRemotePeer.java
@@ -138,7 +138,7 @@ public class CommonsNetFtpRemotePeer extends AbstractRemoteStorage {
         try {
             client = getFTPClient();
             try {
-                if (client.changeWorkingDirectory(PathHelper.getDirName(path))) {
+                if (client.changeWorkingDirectory(PathHelper.concatPaths(getRemoteUrlAsUrl().getPath(),PathHelper.getDirName(path)))) {
                     FTPFile[] fileList = client.listFiles(PathHelper.getFileName(path));
                     if (fileList.length == 1) {
                         return constructItemPropertiesFromGetResponse(path, originatingUrlString, fileList[0]);
@@ -173,7 +173,7 @@ public class CommonsNetFtpRemotePeer extends AbstractRemoteStorage {
         try {
             client = getFTPClient();
             try {
-                if (client.changeWorkingDirectory(PathHelper.getDirName(path))) {
+                if (client.changeWorkingDirectory(PathHelper.concatPaths(getRemoteUrlAsUrl().getPath(),PathHelper.getDirName(path)))) {
                     FTPFile[] fileList = client.listFiles(PathHelper.getFileName(path));
                     if (fileList.length == 1) {
                         FTPFile ftpFile = fileList[0];
