4bd6d778187a753c9611886f90d400cbf853a027
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-08-18 03:52:56 +0000

    Added px-core-xfire module, initial (test) implementations also added

---
+++ ismicro-proximity/px-core-xfire/src/main/java/hu/ismicro/commons/proximity/xfire/SearchService.java
@@ -0,0 +1,46 @@
+package hu.ismicro.commons.proximity.xfire;
+
+import hu.ismicro.commons.proximity.AccessDeniedException;
+import hu.ismicro.commons.proximity.ItemProperties;
+import hu.ismicro.commons.proximity.NoSuchRepositoryException;
+import hu.ismicro.commons.proximity.ProximityException;
+import hu.ismicro.commons.proximity.ProximityRequest;
+
+public interface SearchService {
+
+    /**
+     * Returns an aggregated List of all item properties in all configured
+     * Repositories. It will ALWAYS return List, at least a 0 length list. Will
+     * not return null or throw exception in normal circumstances.
+     * 
+     * @param path
+     * @return list of ItemProperties, possibly 0 length.
+     */
+    ItemProperties[] listItems(ProximityRequest request) throws AccessDeniedException, NoSuchRepositoryException;
+
+    /**
+     * Lists the searchable keywords as returned by Indexer.
+     * 
+     * @return the list of keywords usable in queries.
+     */
+    ItemProperties[] getSearchableKeywords();
+
+    /**
+     * Searches for item.
+     * 
+     * @param example
+     * @return List of ItemProperties, possibly 0 length.
+     */
+    ItemProperties[] searchItem(ItemProperties example) throws ProximityException;
+
+    /**
+     * Searches for item.
+     * 
+     * @param query,
+     *            dependent on indexer backend since Proximity just "passes" it
+     *            out
+     * @return List of ItemProperties, possibly 0 length.
+     */
+    ItemProperties[] searchItem(String query) throws ProximityException;
+
+}
