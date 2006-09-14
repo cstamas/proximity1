a2c557329021245c260c1ef84780df29b04e4947
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-09-14 22:14:46 +0000

    Extended servlet with Spring awareness

--- proximity/px-webapp-webdav/src/main/java/net/sf/webdav/WebdavStoreFactory.java
+++ proximity/px-webapp-webdav/src/main/java/net/sf/webdav/WebdavStoreFactory.java
@@ -1,17 +1,15 @@
 package net.sf.webdav;
 
-import javax.servlet.ServletContext;
-
 public class WebdavStoreFactory {
 
-	private Class fImplementation;
+    private Class fImplementation;
 
-	public WebdavStoreFactory(Class class1) {
-		fImplementation = class1;
-	}
+    public WebdavStoreFactory(Class class1) {
+        fImplementation = class1;
+    }
 
-	public IWebdavStorage getStore(ServletContext ctx) throws InstantiationException, IllegalAccessException {
-		return (IWebdavStorage) fImplementation.newInstance();
-	}
+    public IWebdavStorage getStore() throws InstantiationException, IllegalAccessException {
+        return (IWebdavStorage) fImplementation.newInstance();
+    }
 
 }
