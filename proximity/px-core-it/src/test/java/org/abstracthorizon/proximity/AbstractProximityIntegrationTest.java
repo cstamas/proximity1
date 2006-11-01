d5f2aef9ea38651920221fdb8822213cc46eece7
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-11-01 00:11:26 +0000

    Source reformat.

--- proximity/px-core-it/src/test/java/org/abstracthorizon/proximity/AbstractProximityIntegrationTest.java
+++ proximity/px-core-it/src/test/java/org/abstracthorizon/proximity/AbstractProximityIntegrationTest.java
@@ -4,8 +4,8 @@ import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 public abstract class AbstractProximityIntegrationTest extends AbstractDependencyInjectionSpringContextTests {
 
-    protected String[] getConfigLocations() {
-        return new String[] { "/org/abstracthorizon/proximity/applicationContext.xml" };
-    }
+	protected String[] getConfigLocations() {
+		return new String[] { "/org/abstracthorizon/proximity/applicationContext.xml" };
+	}
 
 }
--- proximity/px-core-it/src/test/java/org/abstracthorizon/proximity/IndexerIntegrationTest.java
+++ proximity/px-core-it/src/test/java/org/abstracthorizon/proximity/IndexerIntegrationTest.java
@@ -6,29 +6,29 @@ import org.abstracthorizon.proximity.indexer.Indexer;
 
 public class IndexerIntegrationTest extends AbstractProximityIntegrationTest {
 
-    private Indexer indexer;
+	private Indexer indexer;
 
-    public void setIndexer(Indexer indexer) {
-        this.indexer = indexer;
-    }
+	public void setIndexer(Indexer indexer) {
+		this.indexer = indexer;
+	}
 
-    public Indexer getIndexer() {
-        return indexer;
-    }
+	public Indexer getIndexer() {
+		return indexer;
+	}
 
-    public void testSearchByExample1() throws ProximityException {
-        HashMapItemPropertiesImpl example = new HashMapItemPropertiesImpl();
-        example.setName("antlr-2.7.5.jar");
-        List result = indexer.searchByItemPropertiesExample(example);
-        logger.info("Got search result:" + result);
-        example = new HashMapItemPropertiesImpl();
-        example.setName("antlr*");
-        result = indexer.searchByItemPropertiesExample(example);
-        logger.info("Got search result:" + result);
-        example = new HashMapItemPropertiesImpl();
-        example.setDirectoryPath("/ismicro/jars");
-        result = indexer.searchByItemPropertiesExample(example);
-        logger.info("Got search result:" + result);
-    }
+	public void testSearchByExample1() throws ProximityException {
+		HashMapItemPropertiesImpl example = new HashMapItemPropertiesImpl();
+		example.setName("antlr-2.7.5.jar");
+		List result = indexer.searchByItemPropertiesExample(example);
+		logger.info("Got search result:" + result);
+		example = new HashMapItemPropertiesImpl();
+		example.setName("antlr*");
+		result = indexer.searchByItemPropertiesExample(example);
+		logger.info("Got search result:" + result);
+		example = new HashMapItemPropertiesImpl();
+		example.setDirectoryPath("/ismicro/jars");
+		result = indexer.searchByItemPropertiesExample(example);
+		logger.info("Got search result:" + result);
+	}
 
 }
