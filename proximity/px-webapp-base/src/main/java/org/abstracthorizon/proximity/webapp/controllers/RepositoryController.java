575910e0e673634655a09464ba8355c6b2ddac00
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-11-01 00:15:38 +0000

    Source reformat.

--- proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/RepositoryController.java
+++ proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/RepositoryController.java
@@ -1,6 +1,5 @@
 package org.abstracthorizon.proximity.webapp.controllers;
 
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -25,89 +24,89 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class RepositoryController extends MultiActionController {
 
-    private Proximity proximity;
+	private Proximity proximity;
 
-    public void setProximity(Proximity proximity) {
-        this.proximity = proximity;
-    }
+	public void setProximity(Proximity proximity) {
+		this.proximity = proximity;
+	}
 
-    public Proximity getProximity() {
-        return proximity;
-    }
+	public Proximity getProximity() {
+		return proximity;
+	}
 
-    public ModelAndView repositoryList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String requestURI = request.getRequestURI().substring(
-                request.getContextPath().length() + request.getServletPath().length());
-        if (requestURI.length() == 0) {
-            requestURI = "/";
-        }
-        logger.debug("Got repository request on URI " + requestURI);
-        String orderBy = request.getParameter("orderBy") == null ? "name" : request.getParameter("orderBy");
-        String targetRepository = request.getParameter("repositoryId");
-        String targetGroup = request.getParameter("repositoryGroupId");
+	public ModelAndView repositoryList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String requestURI = request.getRequestURI().substring(
+				request.getContextPath().length() + request.getServletPath().length());
+		if (requestURI.length() == 0) {
+			requestURI = "/";
+		}
+		logger.debug("Got repository request on URI " + requestURI);
+		String orderBy = request.getParameter("orderBy") == null ? "name" : request.getParameter("orderBy");
+		String targetRepository = request.getParameter("repositoryId");
+		String targetGroup = request.getParameter("repositoryGroupId");
 
-        Item item = null;
-        ProximityRequest pRequest = new ProximityRequest();
-        pRequest.setPath(requestURI);
-        pRequest.setTargetedReposId(targetRepository);
-        pRequest.setTargetedReposGroupId(targetGroup);
-        pRequest.setGrantee(null);
-        pRequest.getAttributes().put(ProximityRequest.REQUEST_REMOTE_ADDRESS, request.getRemoteAddr());
-        
-        // issue #42, collect header information
-        Enumeration headerNames = request.getHeaderNames();
-        while(headerNames.hasMoreElements()) {
-            String headerName = (String) headerNames.nextElement();
-            pRequest.getAttributes().put("http." + headerName.toLowerCase(), request.getHeader(headerName));
-        }
-        
-        try {
-            logger.debug("Got request for " + targetRepository + " repository on URI: " + requestURI);
-            item = proximity.retrieveItem(pRequest);
-            logger.debug("Got response " + item.getProperties().getPath());
+		Item item = null;
+		ProximityRequest pRequest = new ProximityRequest();
+		pRequest.setPath(requestURI);
+		pRequest.setTargetedReposId(targetRepository);
+		pRequest.setTargetedReposGroupId(targetGroup);
+		pRequest.setGrantee(null);
+		pRequest.getAttributes().put(ProximityRequest.REQUEST_REMOTE_ADDRESS, request.getRemoteAddr());
 
-            if (item.getProperties().isDirectory()) {
-                List items = null;
-                items = proximity.listItems(pRequest);
-                PropertyComparator.sort(items, new MutableSortDefinition(orderBy, true, true));
-                Map result = new HashMap();
-                result.put("items", items);
-                result.put("orderBy", orderBy);
-                result.put("requestUri", requestURI);
-                result.put("requestPathList", explodeUriToList(requestURI));
-                return new ModelAndView("repository/repositoryList", result);
-            } else {
-                // TODO: check for If-Modified-Since?
-                response.setContentType("application/octet-stream");
-                response.setContentLength((int) item.getProperties().getSize());
-                response.setDateHeader("Last-Modified", item.getProperties().getLastModified().getTime());
-                InputStream is = item.getStream();
-                OutputStream os = response.getOutputStream();
-                IOUtils.copy(is, os);
-                is.close();
-                return null;
-            }
-        } catch (ItemNotFoundException ex) {
-            logger.info("Item not found on URI " + requestURI);
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return null;
-        } catch (AccessDeniedException ex) {
-            logger.info("Access forbidden to " + requestURI + " for " + request.getRemoteAddr(), ex);
-            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-            return null;
-        }
-    }
+		// issue #42, collect header information
+		Enumeration headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = (String) headerNames.nextElement();
+			pRequest.getAttributes().put("http." + headerName.toLowerCase(), request.getHeader(headerName));
+		}
 
-    protected List explodeUriToList(String uri) {
-        List result = new ArrayList();
-        String[] explodedUri = uri.split("/");
-        // StringBuffer sb = new StringBuffer("/");
-        for (int i = 0; i < explodedUri.length; i++) {
-            if (explodedUri[i].length() > 0) {
-                result.add(explodedUri[i]);
-            }
-        }
-        return result;
-    }
+		try {
+			logger.debug("Got request for " + targetRepository + " repository on URI: " + requestURI);
+			item = proximity.retrieveItem(pRequest);
+			logger.debug("Got response " + item.getProperties().getPath());
+
+			if (item.getProperties().isDirectory()) {
+				List items = null;
+				items = proximity.listItems(pRequest);
+				PropertyComparator.sort(items, new MutableSortDefinition(orderBy, true, true));
+				Map result = new HashMap();
+				result.put("items", items);
+				result.put("orderBy", orderBy);
+				result.put("requestUri", requestURI);
+				result.put("requestPathList", explodeUriToList(requestURI));
+				return new ModelAndView("repository/repositoryList", result);
+			} else {
+				// TODO: check for If-Modified-Since?
+				response.setContentType("application/octet-stream");
+				response.setContentLength((int) item.getProperties().getSize());
+				response.setDateHeader("Last-Modified", item.getProperties().getLastModified().getTime());
+				InputStream is = item.getStream();
+				OutputStream os = response.getOutputStream();
+				IOUtils.copy(is, os);
+				is.close();
+				return null;
+			}
+		} catch (ItemNotFoundException ex) {
+			logger.info("Item not found on URI " + requestURI);
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+		} catch (AccessDeniedException ex) {
+			logger.info("Access forbidden to " + requestURI + " for " + request.getRemoteAddr(), ex);
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			return null;
+		}
+	}
+
+	protected List explodeUriToList(String uri) {
+		List result = new ArrayList();
+		String[] explodedUri = uri.split("/");
+		// StringBuffer sb = new StringBuffer("/");
+		for (int i = 0; i < explodedUri.length; i++) {
+			if (explodedUri[i].length() > 0) {
+				result.add(explodedUri[i]);
+			}
+		}
+		return result;
+	}
 
 }
--- proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/SupportController.java
+++ proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/SupportController.java
@@ -17,112 +17,110 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class SupportController extends MultiActionController {
 
-    private Proximity proximity;
+	private Proximity proximity;
 
-    private Indexer indexer;
+	private Indexer indexer;
 
-    public void setProximity(Proximity proximity) {
-        this.proximity = proximity;
-    }
+	public void setProximity(Proximity proximity) {
+		this.proximity = proximity;
+	}
 
-    public Proximity getProximity() {
-        return proximity;
-    }
+	public Proximity getProximity() {
+		return proximity;
+	}
 
-    public Indexer getIndexer() {
-        return indexer;
-    }
+	public Indexer getIndexer() {
+		return indexer;
+	}
 
-    public void setIndexer(Indexer indexer) {
-        this.indexer = indexer;
-    }
+	public void setIndexer(Indexer indexer) {
+		this.indexer = indexer;
+	}
 
-    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.debug("Got request for index");
-        return new ModelAndView("index");
-    }
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.debug("Got request for index");
+		return new ModelAndView("index");
+	}
 
-    public ModelAndView search(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.debug("Got request for search");
-        ItemProperties example = null;
-        String query = null;
+	public ModelAndView search(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.debug("Got request for search");
+		ItemProperties example = null;
+		String query = null;
 
-        if (ServletRequestUtils.getStringParameter(request, "searchAllRegexp") != null) {
+		if (ServletRequestUtils.getStringParameter(request, "searchAllRegexp") != null) {
 
-            example = new HashMapItemPropertiesImpl();
-            example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchAllRegexp") + "*");
+			example = new HashMapItemPropertiesImpl();
+			example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchAllRegexp") + "*");
 
-        } else if (ServletRequestUtils.getStringParameter(request, "searchRepositoryRegexp") != null
-                && ServletRequestUtils.getRequiredStringParameter(request, "searchRepositoryId") != null) {
+		} else if (ServletRequestUtils.getStringParameter(request, "searchRepositoryRegexp") != null
+				&& ServletRequestUtils.getRequiredStringParameter(request, "searchRepositoryId") != null) {
 
-            example = new HashMapItemPropertiesImpl();
-            example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchRepositoryRegexp") + "*");
-            example.setRepositoryId(ServletRequestUtils
-                    .getRequiredStringParameter(request, "searchRepositoryId"));
+			example = new HashMapItemPropertiesImpl();
+			example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchRepositoryRegexp") + "*");
+			example.setRepositoryId(ServletRequestUtils.getRequiredStringParameter(request, "searchRepositoryId"));
 
-        } else if (ServletRequestUtils.getStringParameter(request, "searchGroupRegexp") != null
-                && ServletRequestUtils.getRequiredStringParameter(request, "searchGroupId") != null) {
+		} else if (ServletRequestUtils.getStringParameter(request, "searchGroupRegexp") != null
+				&& ServletRequestUtils.getRequiredStringParameter(request, "searchGroupId") != null) {
 
-            example = new HashMapItemPropertiesImpl();
-            example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchGroupRegexp") + "*");
-            example.setRepositoryGroupId(ServletRequestUtils
-                    .getRequiredStringParameter(request, "searchGroupId"));
+			example = new HashMapItemPropertiesImpl();
+			example.setName(ServletRequestUtils.getRequiredStringParameter(request, "searchGroupRegexp") + "*");
+			example.setRepositoryGroupId(ServletRequestUtils.getRequiredStringParameter(request, "searchGroupId"));
 
-        } else if (ServletRequestUtils.getStringParameter(request, "searchLQL") != null
-                && ServletRequestUtils.getRequiredStringParameter(request, "searchLQLQuery") != null) {
+		} else if (ServletRequestUtils.getStringParameter(request, "searchLQL") != null
+				&& ServletRequestUtils.getRequiredStringParameter(request, "searchLQLQuery") != null) {
 
-            query = ServletRequestUtils.getRequiredStringParameter(request, "searchLQLQuery");
+			query = ServletRequestUtils.getRequiredStringParameter(request, "searchLQLQuery");
 
-        }
+		}
 
-        logger.debug("example=" + (example == null ? null : example.getName()) + ", query=" + query);
+		logger.debug("example=" + (example == null ? null : example.getName()) + ", query=" + query);
 
-        Map context = new HashMap();
+		Map context = new HashMap();
 
-        if (example != null) {
-            List results = getIndexer().searchByItemPropertiesExample(example);
-            context.put("results", results);
-        } else if (query != null) {
-            List results = getIndexer().searchByQuery(query);
-            context.put("results", results);
-        }
-        context.put("searchableKeywords", getIndexer().getSearchableKeywords());
-        context.put("repositories", getProximity().getRepositoryIds());
-        context.put("groups", getProximity().getRepositoryGroupIds());
-        return new ModelAndView("search", context);
-    }
+		if (example != null) {
+			List results = getIndexer().searchByItemPropertiesExample(example);
+			context.put("results", results);
+		} else if (query != null) {
+			List results = getIndexer().searchByQuery(query);
+			context.put("results", results);
+		}
+		context.put("searchableKeywords", getIndexer().getSearchableKeywords());
+		context.put("repositories", getProximity().getRepositoryIds());
+		context.put("groups", getProximity().getRepositoryGroupIds());
+		return new ModelAndView("search", context);
+	}
 
-    public ModelAndView maintenance(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.debug("Got request for maintenance");
-        if (ServletRequestUtils.getStringParameter(request, "reindexAll") != null) {
-            proximity.reindex();
-        } else if (ServletRequestUtils.getStringParameter(request, "reindexSelected") != null
-                && ServletRequestUtils.getRequiredStringParameter(request, "reindexSelectedRepos") != null) {
-            proximity.reindex(ServletRequestUtils.getRequiredStringParameter(request, "reindexSelectedRepos"));
-        }
+	public ModelAndView maintenance(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.debug("Got request for maintenance");
+		if (ServletRequestUtils.getStringParameter(request, "reindexAll") != null) {
+			proximity.reindex();
+		} else if (ServletRequestUtils.getStringParameter(request, "reindexSelected") != null
+				&& ServletRequestUtils.getRequiredStringParameter(request, "reindexSelectedRepos") != null) {
+			proximity.reindex(ServletRequestUtils.getRequiredStringParameter(request, "reindexSelectedRepos"));
+		}
 
-        Map context = new HashMap();
-        List repositories = getProximity().getRepositories();
-        context.put("repositories", repositories);
-        return new ModelAndView("maintenance", context);
-    }
+		Map context = new HashMap();
+		List repositories = getProximity().getRepositories();
+		context.put("repositories", repositories);
+		return new ModelAndView("maintenance", context);
+	}
 
-    public ModelAndView stats(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.debug("Got request for stats");
-        // TODO: refactor this
-        Map stats = new HashMap(); // proximity.getStatistics();
-        Map context = new HashMap();
-        context.put("stats", stats);
-        return new ModelAndView("stats", context);
-    }
+	public ModelAndView stats(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.debug("Got request for stats");
+		// TODO: refactor this
+		Map stats = new HashMap(); // proximity.getStatistics();
+		Map context = new HashMap();
+		context.put("stats", stats);
+		return new ModelAndView("stats", context);
+	}
 
-    public ModelAndView repositories(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        logger.debug("Got request for repositories layout");
-        Map context = new HashMap();
-        context.put("emergeRepositoryGroups", Boolean.valueOf(proximity.isEmergeRepositoryGroups()));
-        context.put("repositories", proximity.getRepositories());
-        context.put("repositoryGroups", proximity.getRepositoryGroups());
-        return new ModelAndView("repositories", "context", context);
-    }
+	public ModelAndView repositories(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.debug("Got request for repositories layout");
+		Map context = new HashMap();
+		context.put("emergeRepositoryGroups", Boolean.valueOf(proximity.isEmergeRepositoryGroups()));
+		context.put("repositories", proximity.getRepositories());
+		context.put("repositoryGroups", proximity.getRepositoryGroups());
+		return new ModelAndView("repositories", "context", context);
+	}
 
 }
