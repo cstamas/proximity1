aaa37ada86778809a560aa8277298a66a4bb7ae4
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-11-01 00:14:38 +0000

    Fix for issue #71. And by my stupidity, source reformat. Sorry!

--- px-core-maven/src/main/java/org/abstracthorizon/proximity/maven/MavenArtifactRecognizer.java
+++ px-core-maven/src/main/java/org/abstracthorizon/proximity/maven/MavenArtifactRecognizer.java
@@ -17,52 +17,52 @@ package org.abstracthorizon.proximity.maven;
  */
 public class MavenArtifactRecognizer {
 
-    /**
-     * Is this item M1/M2 Checksum?
-     * 
-     * @param name
-     *            the full name of item.
-     * @return true if it is Checksum, false otherwise.
-     */
-    public static boolean isChecksum(String name) {
-        return name.endsWith(".sha1") || name.endsWith(".md5");
-    }
+	/**
+	 * Is this item M1/M2 Checksum?
+	 * 
+	 * @param name
+	 *            the full name of item.
+	 * @return true if it is Checksum, false otherwise.
+	 */
+	public static boolean isChecksum(String name) {
+		return name.endsWith(".sha1") || name.endsWith(".md5");
+	}
 
-    /**
-     * Is this item M1/M2 POM?
-     * 
-     * @param name
-     *            the full name of item.
-     * @return true if it is M1/M2 POM, false otherwise.
-     */
-    public static boolean isPom(String name) {
-        return name.endsWith(".pom") || name.endsWith(".pom.sha1") || name.endsWith(".pom.md5");
-    }
+	/**
+	 * Is this item M1/M2 POM?
+	 * 
+	 * @param name
+	 *            the full name of item.
+	 * @return true if it is M1/M2 POM, false otherwise.
+	 */
+	public static boolean isPom(String name) {
+		return name.endsWith(".pom") || name.endsWith(".pom.sha1") || name.endsWith(".pom.md5");
+	}
 
-    /**
-     * Is this item M1/M2 snapshot?
-     * 
-     * @param name
-     *            the full name of item.
-     * @return true if it is M1/M2 snapshot, false otherwise.
-     */
-    public static boolean isSnapshot(String dir, String name) {
-        // TODO: review SNAPSHOT recognition, in M2 thins changed!
-        // the file is in form:
-        // appassembler-maven-plugin-1.0-20060714.142547-1.pom
-        return dir.indexOf("SNAPSHOT") != -1 || name.indexOf("SNAPSHOT") != -1;
-    }
+	/**
+	 * Is this item M1/M2 snapshot?
+	 * 
+	 * @param name
+	 *            the full name of item.
+	 * @return true if it is M1/M2 snapshot, false otherwise.
+	 */
+	public static boolean isSnapshot(String dir, String name) {
+		// TODO: review SNAPSHOT recognition, in M2 thins changed!
+		// the file is in form:
+		// appassembler-maven-plugin-1.0-20060714.142547-1.pom
+		return dir.indexOf("SNAPSHOT") != -1 || name.indexOf("SNAPSHOT") != -1;
+	}
 
-    /**
-     * Is this item M2 metadata?
-     * 
-     * @param name
-     *            the full name of item.
-     * @return true if it is M2 metadata, false otherwise.
-     */
-    public static boolean isMetadata(String name) {
-        return name.endsWith("maven-metadata.xml") || name.endsWith("maven-metadata.xml.sha1")
-                || name.endsWith("maven-metadata.xml.md5");
-    }
+	/**
+	 * Is this item M2 metadata?
+	 * 
+	 * @param name
+	 *            the full name of item.
+	 * @return true if it is M2 metadata, false otherwise.
+	 */
+	public static boolean isMetadata(String name) {
+		return name.endsWith("maven-metadata.xml") || name.endsWith("maven-metadata.xml.sha1")
+				|| name.endsWith("maven-metadata.xml.md5");
+	}
 
 }
