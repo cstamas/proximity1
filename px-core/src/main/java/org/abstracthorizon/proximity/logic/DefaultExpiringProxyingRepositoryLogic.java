1225c9c4a7f28ac9dbdf66282eaa911c09a11ddb
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-11-09 03:46:07 +0000

    Fixing issue #78

--- px-core/src/main/java/org/abstracthorizon/proximity/logic/DefaultExpiringProxyingRepositoryLogic.java
+++ px-core/src/main/java/org/abstracthorizon/proximity/logic/DefaultExpiringProxyingRepositoryLogic.java
@@ -1,9 +1,6 @@
 package org.abstracthorizon.proximity.logic;
 
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.abstracthorizon.proximity.Item;
 import org.abstracthorizon.proximity.ProximityRequest;
@@ -25,18 +22,6 @@ public class DefaultExpiringProxyingRepositoryLogic extends DefaultProxyingRepos
 	public static final String METADATA_EXPIRES = "item.expires";
 
 	private long itemExpirationPeriod = 86400 * 1000; // 24 hours
-
-	private long notFoundCachePeriod = 86400 * 1000; // 24 hours
-
-	private Map notFoundCache = Collections.synchronizedMap(new HashMap());
-
-	public long getNotFoundCachePeriodInSeconds() {
-		return notFoundCachePeriod / 1000;
-	}
-
-	public void setNotFoundCachePeriodInSeconds(long notFoundCachePeriod) {
-		this.notFoundCachePeriod = notFoundCachePeriod * 1000;
-	}
 
 	public long getItemExpirationPeriodInSeconds() {
 		return itemExpirationPeriod / 1000;
@@ -68,33 +53,6 @@ public class DefaultExpiringProxyingRepositoryLogic extends DefaultProxyingRepos
 		return item;
 	}
 
-	public boolean shouldCheckForRemoteCopy(Repository repository, ProximityRequest request, Item localItem) {
-		if (localItem == null) {
-			String requestKey = getRepositoryRequestAsKey(repository, request);
-			if (notFoundCache.containsKey(requestKey)) {
-				// it is in cache, check when it got in
-				Date lastRequest = (Date) notFoundCache.get(requestKey);
-				if (lastRequest.before(new Date(System.currentTimeMillis() - notFoundCachePeriod))) {
-					// the notFoundCache record expired, remove it and check its
-					// existence
-					logger.debug("n-cache record expired, will go again remote to fetch.");
-					notFoundCache.remove(requestKey);
-					return true;
-				} else {
-					// the notFoundCache record is still valid, do not check its
-					// existence
-					logger.debug("n-cache record still active, will not go remote to fetch.");
-					return false;
-				}
-			} else {
-				// it is not in notFoundCache, check its existence
-				return true;
-			}
-		} else {
-			return super.shouldCheckForRemoteCopy(repository, request, localItem);
-		}
-	}
-
 	/**
 	 * If expiration period is not NO_EXPIRATION, it will apply it on all items.
 	 */
@@ -106,34 +64,6 @@ public class DefaultExpiringProxyingRepositoryLogic extends DefaultProxyingRepos
 					Long.toString(expires.getTime()));
 		}
 		return remoteItem;
-	}
-
-	public Item afterRetrieval(Repository repository, ProximityRequest request, Item localItem, Item remoteItem) {
-		Item item = super.afterRetrieval(repository, request, localItem, remoteItem);
-		if (item == null) {
-			// we have not found it
-			// put the path into not found cache
-			String requestPath = getRepositoryRequestAsKey(repository, request);
-			if (!notFoundCache.containsKey(requestPath)) {
-				logger.info("Caching failed request [{}] to n-cache.", requestPath);
-				notFoundCache.put(requestPath, new Date());
-			}
-		}
-		return item;
-	}
-
-	/**
-	 * Constructs a unique request key using repoId and request path.
-	 * 
-	 * @param repository
-	 * @param request
-	 * @return a unique key in form "repoId:/path/to/artifact"
-	 */
-	protected String getRepositoryRequestAsKey(Repository repository, ProximityRequest request) {
-		StringBuffer sb = new StringBuffer(repository.getId());
-		sb.append(":");
-		sb.append(request.getPath());
-		return sb.toString();
 	}
 
 }
