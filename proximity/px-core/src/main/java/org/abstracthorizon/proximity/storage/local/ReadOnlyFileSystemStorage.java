b93b35dee98a2672f09d846566f7b00b9325fd28
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2006-11-28 19:02:25 +0000

    Fixing issue #92, forcing last-modified property item to come from File and not (possibly outdated) metadata.

--- proximity/px-core/src/main/java/org/abstracthorizon/proximity/storage/local/ReadOnlyFileSystemStorage.java
+++ proximity/px-core/src/main/java/org/abstracthorizon/proximity/storage/local/ReadOnlyFileSystemStorage.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -109,6 +110,7 @@ public class ReadOnlyFileSystemStorage extends AbstractLocalStorage {
 				if (target.isFile()) {
 					result.setStream(new FileInputStream(target));
 					properties.setSize(target.length());
+					properties.setLastModified(new Date(target.lastModified()));
 				}
 			}
 			return result;
