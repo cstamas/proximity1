31bcd12b11c8809e53aea849b1f6bdec9934aac0
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2007-02-08 20:25:56 +0000

    Applied patch from issue #95. Thanx ehsavoie.

--- px-core/src/main/java/org/abstracthorizon/proximity/storage/local/WritableFileSystemStorage.java
+++ px-core/src/main/java/org/abstracthorizon/proximity/storage/local/WritableFileSystemStorage.java
@@ -4,8 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 import org.abstracthorizon.proximity.Item;
+import org.abstracthorizon.proximity.ItemProperties;
+import org.abstracthorizon.proximity.logic.DefaultExpiringProxyingRepositoryLogic;
 import org.abstracthorizon.proximity.storage.StorageException;
 import org.apache.commons.io.IOUtils;
 
@@ -26,11 +32,12 @@ public class WritableFileSystemStorage extends ReadOnlyFileSystemStorage {
 		if (!item.getProperties().isFile()) {
 			throw new IllegalArgumentException("Only files can be stored!");
 		}
-		logger.debug("Storing item in [{}] in storage directory {}", item.getProperties().getPath(),
-				getStorageBaseDir());
+		logger.debug("Storing item in [{}] in storage directory {}", item
+				.getProperties().getPath(), getStorageBaseDir());
 		try {
 			if (item.getStream() != null) {
-				File file = new File(getStorageBaseDir(), item.getProperties().getPath());
+				File file = new File(getStorageBaseDir(), item.getProperties()
+						.getPath());
 				file.getParentFile().mkdirs();
 				FileOutputStream os = new FileOutputStream(file);
 				IOUtils.copy(item.getStream(), os);
@@ -38,23 +45,28 @@ public class WritableFileSystemStorage extends ReadOnlyFileSystemStorage {
 				os.flush();
 				os.close();
 				item.setStream(new FileInputStream(file));
-				file.setLastModified(item.getProperties().getLastModified().getTime());
+				file.setLastModified(item.getProperties().getLastModified()
+						.getTime());
 				if (isMetadataAware()) {
-					getProxiedItemPropertiesFactory().expandItemProperties(item.getProperties().getPath(), file, false);
+					getProxiedItemPropertiesFactory().expandItemProperties(
+							item.getProperties().getPath(), file, false);
 					storeItemProperties(item.getProperties());
 				}
 			}
 		} catch (IOException ex) {
-			throw new StorageException("IOException in FS storage " + getStorageBaseDir(), ex);
+			throw new StorageException("IOException in FS storage "
+					+ getStorageBaseDir(), ex);
 		}
 	}
 
 	public void deleteItem(String path) throws StorageException {
-		logger.info("Deleting [{}] from storage directory {}", path, getStorageBaseDir());
+		logger.info("Deleting [{}] from storage directory {}", path,
+				getStorageBaseDir());
 		File file = new File(getStorageBaseDir(), path);
 		if (file.exists()) {
 			if (!file.delete()) {
-				throw new StorageException("Unable to delete file " + file.getPath());
+				throw new StorageException("Unable to delete file "
+						+ file.getPath());
 			}
 			if (isMetadataAware()) {
 				deleteItemProperties(path);
@@ -63,11 +75,57 @@ public class WritableFileSystemStorage extends ReadOnlyFileSystemStorage {
 	}
 
 	public void deleteItemProperties(String path) throws StorageException {
-		logger.debug("Deleting [{}] metadata from metadata directory {}", path, getMetadataBaseDir());
+		logger.debug("Deleting [{}] metadata from metadata directory {}", path,
+				getMetadataBaseDir());
 		File file = new File(getMetadataBaseDir(), path);
 		if (file.exists() && !file.delete()) {
-			throw new StorageException("Unable to delete file " + file.getPath());
+			throw new StorageException("Unable to delete file "
+					+ file.getPath());
 		}
 	}
 
+	public void recreateMetadata(Map extraProps) throws StorageException {
+
+		// issue #44, we will not delete existing metadata,
+		// instead, we will force to "recreate" those the properties factory
+		// eventually appending it with new ones.
+
+		int processed = 0;
+		Stack stack = new Stack();
+		List dir = listItems(ItemProperties.PATH_ROOT);
+		stack.push(dir);
+		while (!stack.isEmpty()) {
+			dir = (List) stack.pop();
+			for (Iterator i = dir.iterator(); i.hasNext();) {
+				ItemProperties ip = (ItemProperties) i.next();
+
+				if (ip.isDirectory()) {
+					List subdir = listItems(ip.getPath());
+					stack.push(subdir);
+				} else {
+					logger.debug("**** {}", ip.getPath());
+					File target = new File(getStorageBaseDir(), ip.getPath());
+					ItemProperties nip = getProxiedItemPropertiesFactory()
+							.expandItemProperties(ip.getPath(), target, false);
+					if (ip.getMetadata(DefaultExpiringProxyingRepositoryLogic.METADATA_EXPIRES) != null) {
+						logger.debug("We have an "
+										+ DefaultExpiringProxyingRepositoryLogic.METADATA_EXPIRES
+										+ " property");
+						nip.setMetadata(
+							DefaultExpiringProxyingRepositoryLogic.METADATA_EXPIRES,
+							ip.getMetadata(DefaultExpiringProxyingRepositoryLogic.METADATA_EXPIRES));
+					}
+					logger.debug("Recreating metadata : adding " + extraProps
+							+ " to " + nip.getAllMetadata());
+					if (extraProps != null) {
+						nip.getAllMetadata().putAll(extraProps);
+					}
+					storeItemProperties(nip);
+					processed++;
+				}
+			}
+		}
+		logger.info("Recreated metadata on {} items.", Integer
+				.toString(processed));
+	}
 }
