67a66acbce82c9ff41067672519db271e61115db
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2007-02-08 20:05:42 +0000

    Fixed unused import.

--- proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/RepositoryController.java
+++ proximity/px-webapp-base/src/main/java/org/abstracthorizon/proximity/webapp/controllers/RepositoryController.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
