abe0a7196df780091f98e28fb3ba86b5c5990f46
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2007-07-30 12:25:49 +0000

    Refactored jobs to use QuartzJobBean to make config easier. Warning: this is not final solution, this change made an unwanted Spring dependency in this module!

--- proximity/px-core-scheduler/src/main/java/org/abstracthorizon/proximity/scheduler/AbstractProximityJob.java
+++ proximity/px-core-scheduler/src/main/java/org/abstracthorizon/proximity/scheduler/AbstractProximityJob.java
@@ -1,62 +1,30 @@
 package org.abstracthorizon.proximity.scheduler;
 
-import org.abstracthorizon.proximity.Proximity;
-import org.abstracthorizon.proximity.indexer.Indexer;
-import org.abstracthorizon.proximity.stats.StatisticsGatherer;
 import org.quartz.Job;
-import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public abstract class AbstractProximityJob implements Job {
-	
-	public static final String PROXIMITY_KEY = "proximity";
-	public static final String INDEXER_KEY = "indexer";
-	public static final String STATISTICS_GATHERER_KEY = "statisticGatherer";
+public abstract class AbstractProximityJob extends QuartzJobBean implements Job {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private Proximity proximity;
+    protected Logger getLogger() {
+	return this.logger;
+    }
 
-	private Indexer indexer;
-
-	private StatisticsGatherer statisticGatherer;
-
-	protected Logger getLogger() {
-		return this.logger;
+    public void executeInternal(JobExecutionContext context) throws JobExecutionException {
+	getLogger().info("Job {} started.", context.getJobDetail().getFullName());
+	try {
+	    doExecute(context);
+	    getLogger().info("Job {} finished.", context.getJobDetail().getFullName());
+	} catch (Exception ex) {
+	    throw new JobExecutionException(ex);
 	}
+    }
 
-	protected Proximity getProximity() {
-		return this.proximity;
-	}
-
-	protected Indexer getIndexer() {
-		return this.indexer;
-	}
-
-	protected StatisticsGatherer getStatisticsGatherer() {
-		return this.statisticGatherer;
-	}
-
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		
-		JobDataMap jobDataMap = context.getMergedJobDataMap();
-		
-		this.proximity = (Proximity) jobDataMap.get(PROXIMITY_KEY);
-		this.indexer = (Indexer) jobDataMap.get(INDEXER_KEY);
-		this.statisticGatherer = (StatisticsGatherer) jobDataMap.get(STATISTICS_GATHERER_KEY);
-		
-		getLogger().info("Job {} started.", context.getJobDetail().getFullName());
-		try {
-			doExecute(context);
-			getLogger().info("Job {} finished.", context.getJobDetail().getFullName());
-		} catch (Exception ex) {
-			throw new JobExecutionException(ex);
-		}
-	}
-
-	protected abstract void doExecute(JobExecutionContext ctx) throws Exception;
+    protected abstract void doExecute(JobExecutionContext ctx) throws Exception;
 
 }
