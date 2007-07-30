8a6ff12958b3d20eb91fe90bb52442c69d4cf826
cstamas <cstamas@bfc62a11-e7ff-0310-a840-f4f7f8e266be>
2007-07-30 12:25:49 +0000

    Refactored jobs to use QuartzJobBean to make config easier. Warning: this is not final solution, this change made an unwanted Spring dependency in this module!

--- px-core-scheduler/src/main/java/org/abstracthorizon/proximity/scheduler/jobs/RepositoryReindexerJob.java
+++ px-core-scheduler/src/main/java/org/abstracthorizon/proximity/scheduler/jobs/RepositoryReindexerJob.java
@@ -1,22 +1,40 @@
 package org.abstracthorizon.proximity.scheduler.jobs;
 
+import org.abstracthorizon.proximity.indexer.Indexer;
 import org.abstracthorizon.proximity.scheduler.AbstractProximityJob;
 import org.quartz.JobExecutionContext;
 import org.quartz.StatefulJob;
 
 public class RepositoryReindexerJob extends AbstractProximityJob implements StatefulJob {
-	
-	public static final String REPOSITORY_ID_KEY = "repositoryId";
+    
+    private String repositoryId = null;
 
-	protected void doExecute(JobExecutionContext ctx) throws Exception {
-		if (ctx.getMergedJobDataMap().containsKey(REPOSITORY_ID_KEY)) {
-			String repositoryId = (String) ctx.getMergedJobDataMap().getString(REPOSITORY_ID_KEY);
-			getLogger().info("Scheduled reindexing of repository {}", repositoryId);
-			getIndexer().reindex(repositoryId);
-		} else {
-			getLogger().info("Scheduled reindexing all repositories");
-			getIndexer().reindex();
-		}
+    private Indexer indexer;
+
+    public String getRepositoryId() {
+        return repositoryId;
+    }
+
+    public void setRepositoryId(String repositoryId) {
+        this.repositoryId = repositoryId;
+    }
+
+    protected Indexer getIndexer() {
+	return this.indexer;
+    }
+
+    public void setIndexer(Indexer indexer) {
+	this.indexer = indexer;
+    }
+
+    protected void doExecute(JobExecutionContext ctx) throws Exception {
+	if (repositoryId != null) {
+	    getLogger().info("Scheduled reindexing of repository {}", repositoryId);
+	    getIndexer().reindex(repositoryId);
+	} else {
+	    getLogger().info("Scheduled reindexing all repositories");
+	    getIndexer().reindex();
 	}
+    }
 
 }
